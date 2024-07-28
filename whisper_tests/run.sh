6f6ce48854048b7f4d85a8317bba5eb49af738c1
Carlos Peralta <carlos9917@gmail.com>
2024-07-28 20:05:40 +0200

    check in whisper tests

--- whisper_tests/run.sh
+++ whisper_tests/run.sh
@@ -1,5 +1,9 @@
 #whisper.exe demo.mp4 --model medium --language 'Chinese' --output_format txt --task transcribe
 audio="/home/cperalta/Downloads/kapitalet-327-argentina-dynamisk.mp3"
+audio="/home/cperalta/antilop_149.mp3"
+audio="/home/cperalta/Downloads/program_torsdag_7_december_2023_20231207_1536137199.mp3"
+audio="/home/cperalta/Downloads/program_onsdag_6_december_2023_20231206_1429503442.mp3"
+audio="/home/cperalta/Downloads/program_fredag_8_december_2023_20231208_1606000615.mp3"
 whisper $audio --language=Swedish --output_format txt --task transcribe
 
 
